37706e75c46d386fafe916ee8071b601191bb553
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2025-01-16 13:36:39 -0300

    exercises 151 to 152, adding one extra

--- exercises/151-search/app.js
+++ exercises/151-search/app.js
@@ -1,7 +1,49 @@
 function search(array, value) {
   // your code here
-  
+  let stop = Math.floor(array.length /2);
+  if(value <= stop){
+    for(let i = 0; i=stop; i++){
+      if(array[i] == value) {
+        return i;
+      }  
+    }
+  } else {
+    for(let i = stop; i < array.length; i++){
+      if(array[i] == value) {
+        return i;
+    }
+  }
+}
 }
 
 let arr = [1, 3, 16, 22, 31, 33, 34]
 console.log(search(arr, 31)); // --> 4
+
+
+
+
+// function findMissing(array) {
+//   // your code here
+//   for(let i=0; i < array.length; i++){
+//     if((array[i] >0) && (array[i] - array[i-1] > 1)) {
+//       return array[i-1] + 1;
+//     }     
+//   }
+// }
+
+// let arr = [-5,-4,-3,-1, 0, 1, 2, 3, 5, 6, 7, 9]
+// console.log(findMissing(arr)); // --> 4
+
+
+// function findMissing(array) {
+//   // your code here
+//   for(i in array){
+//     if((array[i] >0) && (array[i] - array[i-1] > 1)) {
+//       return array[i-1] + 1;
+//     }     
+//   }
+// }
+
+// let arr = [-5,-4,-3,-1, 0, 1, 2, 3, 5, 6, 7, 9]
+// console.log(findMissing(arr)); // --> 4
+
--- exercises/152-isogram/app.js
+++ exercises/152-isogram/app.js
@@ -1,7 +1,15 @@
 function isIsogram(text) {
   // your code here
-  
+  let arr = text.split("");
+  for(let i=0; i< arr.length; i++){
+    if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])){
+      return false;
+    }
+  } return true;
 }
 
 console.log(isIsogram("Camile")); // --> true
 console.log(isIsogram("Camille")); // --> false
+
+
+// no pude sola T-T. El return truetenbía que estar fuera del bucle, si no el bucle se detiene luego de la primera iteración.
